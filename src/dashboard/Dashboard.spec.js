bd5b7d2f76663b4ad6eb50cf28aca5e0104c18c5
john <john@spinningplates.org>
2019-12-06 17:28:20 -0800

    Display Test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,8 +4,6 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Dashboard from './Dashboard';
-import Controls from '../controls/Controls';
-import Display from '../display/Display';
 
 test('<Dashboard /> snapshot', () => {
   const wrapper = rtl.render(<Dashboard />);
@@ -30,7 +28,7 @@ test('Shows the controls and display', () => {
 
 test('Cannot be Closed or Opened if it is Locked', () => {
   const wrapper = rtl.render(<Dashboard />);
-  const locked = wrapper.queryByText(/unlock gate/i)
+  const locked = wrapper.queryByText(/unlock gate/i);
 
   locked && expect(wrapper.getByText(/locked/i).className).toMatch(/red-led/i);
   locked && expect(wrapper.getByText(/closed/i).className).toMatch(/red-led/i);
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,9 +3,30 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Display from './Display'
+import Display from './Display';
 
+test('Displays if gate is Open/Closed and if it is Locked/Unlocked', () => {
+  const wrapper = rtl.render(<Display />);
 
-// test('displays if gate is open/closed and if it is locked/unlocked', () => {
+  const unlocked_locked = wrapper.getByTestId('unlocked-locked');
+  const open_closed = wrapper.getByTestId('open-closed');
 
-// });
+  expect(unlocked_locked).toBeInTheDocument()
+  expect(open_closed).toBeInTheDocument()
+});
+
+test('Displays Closed if closed props is true and Open if otherwise', () => {
+  const wrapper = rtl.render(<Display />);
+  const closed = wrapper.getByTestId('open-closed')
+
+  closed.classList.contains('red-led') && expect(closed.textContent).toMatch(/closed/i);
+  closed.classList.contains('green-led') && expect(closed.textContent).toMatch(/open/i);
+})
+
+test('Displays Locked if the locked prop is true and Unlocked if otherwise', () => {
+  const wrapper = rtl.render(<Display />);
+  const locked = wrapper.getByTestId('unlocked-locked');
+
+  locked.classList.contains('red-led') && expect(locked.textContent).toMatch(/locked/i);
+  locked.classList.contains('green-led') && expect(locked.textContent).toMatch(/unlocked/i)
+})
